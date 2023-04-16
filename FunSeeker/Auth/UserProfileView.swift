8df4c500bd2fbf9bbc8b1438ceb534152015842d
ChrisMKocabas <muhammedkocabas@outlook.com>
2023-04-16 14:59:40 -0400

    Authentication completed. Signup/Register implemented Login/logout implemented

--- FunSeeker/Auth/UserProfileView.swift
+++ FunSeeker/Auth/UserProfileView.swift
@@ -125,7 +125,7 @@ struct UserProfileView: View {
           .animation(.spring(response:0.55, dampingFraction: 0.7,
                             blendDuration: 0), value: showNewScreen)
           .offset(y: showNewScreen ? 0 : UIScreen.main.bounds.height)
-      } 
+      }
     }
     .navigationTitle("Profile")
     .navigationBarTitleDisplayMode(.inline)
@@ -134,7 +134,7 @@ struct UserProfileView: View {
                         isPresented: $presentingConfirmationDialog, titleVisibility: .visible) {
       Button("Delete Account", role: .destructive, action: deleteAccount)
       Button("Cancel", role: .cancel, action: { })
-    }
+    } 
   }
 }
 
